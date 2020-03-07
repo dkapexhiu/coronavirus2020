70b64c65b49e8f277f9333006efd2495372ce1b8
unknown <daniel.kapexhiu@gmail.com>
2020-03-07 10:02:37 +0100

    added 06-03 data

--- src/DateParser.js
+++ src/DateParser.js
@@ -17,7 +17,7 @@ class DataParser extends Component {
 
   componentWillMount() {
     // Your parse code, but not seperated in a function
-    var csvFilePath = require("./03-05-2020.csv");
+    var csvFilePath = require("./03-06-2020.csv");
     var Papa = require("papaparse");
     Papa.parse(csvFilePath, {
       header: true,
