c50239827cc2a6759635f21ae3d2736a01dc3e1b
Daniel <daniel.kapexhiu@gmail.com>
2020-03-17 09:37:37 +0100

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
     $('#app').append('<div class="container"></div>');
 
-    const url = 'https://covid19-json-api.herokuapp.com/posts';
+    const url = 'https://covid19-json-api.herokuapp.com/data';
 
     $.get(url).done(function(data) {
         data.forEach(movie => {
         	console.log(movie)
             $('.container').append(
                 '<div class="card">'
-                + '<h1>Region: ' + movie.Country.Region + '</h1>'
-                + '<p>State: ' + movie.Province.State + '</p>'
+                + '<h1>Country: ' + movie.Country_Region + '</h1>'
+                + '<p>Active: ' + movie.Active + '</p>'
                 + '<p>Confirmed: ' + movie.Confirmed + '</p>'
                 + '<p>Deaths: ' + movie.Deaths + '</p>'
                 + '<p>Recovered: ' + movie.Recovered + '</p>'
